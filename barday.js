aeceba5d24107054a828799e6240a3ee9ccf5ecf
sansiri <sansiri.siripornlertkul@gmail.com>
2024-05-24 12:14:02 +0700

    Update barday.js

--- barday.js
+++ barday.js
@@ -1,121 +1,50 @@
 import { stockItems } from './stockItems.js';
+import { 
+  getCurrentDateTime, 
+  getFormattedDateTime, 
+  populateTable, 
+  createTableRow, 
+  handleFormSubmission, 
+  filterFilledItems, 
+  populateConfirmationTable, 
+  showModal, 
+  hideModal, 
+  handleFinalSubmit 
+} from './utils.js';
 
 document.addEventListener('DOMContentLoaded', function() {
-  const stockTableBody = document.getElementById('stockTableBody');
   const timestampField = document.getElementById('timestamp');
+  timestampField.value = getCurrentDateTime();
+
   const pageTitle = document.title;
-  const headerTitle = document.querySelector('.kitchen-header');
-  const navigateButton = document.getElementById('navigateButton');
-  const loadingMessage = document.getElementById('loadingMessage');
-  const confirmationModal = document.getElementById('confirmationModal');
-  const confirmationTableBody = document.getElementById('confirmationTableBody');
-  const finalSubmitButton = document.getElementById('finalSubmitButton');
-  const backButton = document.getElementById('backButton');
-  const modalBackdrop = document.getElementById('modalBackdrop');
-  const loadingSpinner = document.getElementById('loadingSpinner');
+  const kitchenType = 'บาร์วัน';
 
-  // Set current date and time
-  const now = new Date();
-  timestampField.value = now.toLocaleString();
+  const filteredItems = stockItems.filter(item => item.kitchen === kitchenType);
+  populateTable(filteredItems, 'stockTableBody', createTableRow);
 
-  // Function to format date and time for the file name
-  function getFormattedDateTime() {
-    const year = now.getFullYear();
-    const month = (now.getMonth() + 1).toString().padStart(2, '0');
-    const day = now.getDate().toString().padStart(2, '0');
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
-    return `${year}${month}${day}-${hours}${minutes}${seconds}`;
-  }
-
-  // Filter stock items for the bar
-  const filteredItems = stockItems.filter(item => item.kitchen === 'บาร์วัน');
-
-  // Populate the table with stock items
-  filteredItems.forEach((item, index) => {
-    const row = document.createElement('tr');
-
-    row.innerHTML = `
-      <td>${index + 1}</td>
-      <td>${item.name}</td>
-      <td>${item.fixedStock}</td>
-      <td><input type="number" name="inventoryCount_${index}" placeholder=""></td>
-      <td><input type="number" name="numberToOrder_${index}" placeholder=""></td>
-      <td><input type="number" name="counting_${index}" placeholder=""></td>
-      <td>${item.type}</td>
-      <td>${item.kitchen}</td>
-    `;
-
-    stockTableBody.appendChild(row);
+  handleFormSubmission('stockForm', filteredItems, filterFilledItems, function(filledItems) {
+    populateConfirmationTable(filledItems, 'confirmationTableBody');
   });
 
-  // Form submission handler
-  document.getElementById('stockForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+  handleFinalSubmit('finalSubmitButton', 'stockForm', filteredItems, function(items, formId) {
+    showLoadingSpinner();
 
-    const formData = new FormData(this);
-    const filledItems = [];
+    const formData = new FormData(document.getElementById(formId));
+    const filledItems = items.map((item, index) => {
+      return {
+        name: item.name,
+        fixedStock: item.fixedStock,
+        inventoryCount: formData.get(`inventoryCount_${index}`) || "",
+        numberToOrder: formData.get(`numberToOrder_${index}`) || "",
+        counting: formData.get(`counting_${index}`) || "",
+        type: item.type,
+        kitchen: item.kitchen
+      };
+    });
 
-    formData.forEach((value, key) => {
-      const [name, index] = key.split('_');
-      if (!filledItems[index]) filledItems[index] = { [name]: value || "" };
-      else filledItems[index][name] = value || "";
-    });
-  
-    // Filter out empty rows
-    const filteredFilledItems = filledItems.filter(item => {
-      return item.inventoryCount || item.numberToOrder || item.counting;
-    });
-  
-    // Populate the confirmation table with filled items
-    confirmationTableBody.innerHTML = '';
-    filteredFilledItems.forEach((item, index) => {
-      const row = document.createElement('tr');
-      row.innerHTML = `
-        <td>${index + 1}</td>
-        <td>${filteredItems[index].name}</td>
-        <td>${filteredItems[index].fixedStock}</td>
-        <td class="bold-text">${item.inventoryCount}</td>
-        <td class="bold-text">${item.numberToOrder}</td>
-        <td class="bold-text">${item.counting}</td>
-
-      `;
-      confirmationTableBody.appendChild(row);
-    });
-  
-    // Show the confirmation modal
-    modalBackdrop.style.display = 'block';
-    confirmationModal.style.display = 'block';
-  });
-  
-  // Final submit button handler
-  finalSubmitButton.addEventListener('click', function() {
-    loadingSpinner.style.display = 'block'; // Show the loading spinner
-  
-    const formData = new FormData(document.getElementById('stockForm'));
-    const data = { items: [] };
-  
-    formData.forEach((value, key) => {
-      const [name, index] = key.split('_');
-      if (!data.items[index]) data.items[index] = { [name]: value || "" };
-      else data.items[index][name] = value || "";
-    });
-  
-    data.items.forEach((item, index) => {
-      item.name = filteredItems[index].name;
-      item.fixedStock = filteredItems[index].fixedStock;
-      item.inventoryCount = item.inventoryCount || "";
-      item.numberToOrder = item.numberToOrder || "";
-      item.counting = item.counting || "";
-      item.type = filteredItems[index].type;
-      item.kitchen = filteredItems[index].kitchen;
-    });
-  
-    // Generate CSV content with header and timestamp
-    const header = `บาร์วัน, ${now.toLocaleString()}\n`;
+    const header = `${kitchenType}, ${getCurrentDateTime()}\n`;
     const columnHeaders = 'ชื่อ,สต็อกที่กำหนด,นับสินค้าคงคลัง,จำนวนที่ต้องการสั่งซื้อ,จำนวนนับ,ประเภท,ครัว\n';
-    const csvContent = header + columnHeaders + data.items.map(item => [
+    const csvContent = header + columnHeaders + filledItems.map(item => [
       `"${item.name}"`,
       `"${item.fixedStock}"`,
       `"${item.inventoryCount}"`,
@@ -124,68 +53,64 @@ document.addEventListener('DOMContentLoaded', function() {
       `"${item.type}"`,
       `"${item.kitchen}"`
     ].join(',')).join('\n');
-  
+
     const blob = new Blob([csvContent], { type: 'text/csv' });
     const reader = new FileReader();
-  
+
     reader.onload = function(event) {
       const base64data = btoa(event.target.result);
       const fileName = `${getFormattedDateTime()}_นับสต๊อกบาร์.csv`;
-  
-      // Create a form to submit the data
+
       const uploadForm = new FormData();
       uploadForm.append('file', base64data);
       uploadForm.append('mimeType', 'text/csv');
       uploadForm.append('filename', fileName);
-  
+
       fetch('https://script.google.com/macros/s/AKfycbx_SZ29mQEXXRPfgAk_xTapQ5LRlOL3O9ZNrTb9A0q_XsuecLkC3M2ivelwncqg-DO-/exec', {
         method: 'POST',
         body: uploadForm
       })
       .then(response => response.json())
       .then(result => {
+        hideLoadingSpinner();
         if (result.url) {
-          // Redirect to the completion page with the file URL and form type as query parameters
           window.location.href = `complete.html?fileUrl=${encodeURIComponent(result.url)}&formType=barday`;
         } else {
-          loadingMessage.style.display = 'none';
           document.getElementById('result').innerHTML = `<p>Error uploading file: ${result.error}</p>`;
-          loadingSpinner.style.display = 'none'; // Hide the loading spinner
         }
       })
       .catch(error => {
-        loadingMessage.style.display = 'none';
-        console.error('Error:', error);
+        hideLoadingSpinner();
         document.getElementById('result').innerHTML = `<p>Error uploading file: ${error}</p>`;
-        loadingSpinner.style.display = 'none'; // Hide the loading spinner
       });
     };
-  
+
     reader.readAsBinaryString(blob);
-  
-    // Hide the confirmation modal
-    modalBackdrop.style.display = 'none';
-    confirmationModal.style.display = 'none';
+    hideModal('modalBackdrop', 'confirmationModal');
   });
-  
-  // Back button handler
-  backButton.addEventListener('click', function() {
-    // Hide the confirmation modal
-    modalBackdrop.style.display = 'none';
-    confirmationModal.style.display = 'none';
+
+  // Show the loading spinner
+  function showLoadingSpinner() {
+    document.getElementById('loadingSpinner').style.display = 'block';
+  }
+
+  // Hide the loading spinner
+  function hideLoadingSpinner() {
+    document.getElementById('loadingSpinner').style.display = 'none';
+  }
+
+  document.getElementById('backButton').addEventListener('click', function() {
+    hideModal('modalBackdrop', 'confirmationModal');
   });
-  
-  // Reset button handler
+
   document.getElementById('resetButton').addEventListener('click', function() {
     if (confirm('คุณแน่ใจหรือว่าต้องการรีเซ็ตแบบฟอร์ม?')) {
       document.getElementById('stockForm').reset();
-      timestampField.value = new Date().toLocaleString(); // Reset timestamp
+      timestampField.value = getCurrentDateTime();
     }
   });
-  
-  // Navigate button handler
-  navigateButton.addEventListener('click', function() {
+
+  document.getElementById('navigateButton').addEventListener('click', function() {
     window.location.href = 'index.html';
   });
-  });
-  
+});
