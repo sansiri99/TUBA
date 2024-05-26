4483298c325fa4576000242d08bec009e2103aa3
sansiri <sansiri.siripornlertkul@gmail.com>
2024-05-27 04:08:51 +0700

    Update thai.js

--- thai.js
+++ thai.js
@@ -112,9 +112,17 @@ document.addEventListener('DOMContentLoaded', function() {
       document.getElementById('stockForm').reset();
       timestampField.value = getCurrentDateTime();
     }
-  });
-
-  document.getElementById('navigateButton').addEventListener('click', function() {
-    window.location.href = 'index.html';
-  });
+    });
+    document.getElementById('navigateButton').addEventListener('click', function() {
+      if (confirm('คุณแน่ใจหรือว่าต้องการไปหน้านับสต๊อก ครัวอิตาเลี่ยน?')) {
+        window.location.href = 'italian.html';
+      }
+    });
+  
+    document.getElementById('homeButton').addEventListener('click', function() {
+      if (confirm('คุณแน่ใจหรือว่าต้องการกลับไปหน้าแรก?')) {
+        window.location.href = 'index.html';
+      }
+    });
+    
 });
